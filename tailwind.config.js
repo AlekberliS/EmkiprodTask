1bb07ee1ebf2222a23df6b429ff7a868ba2dfae2
AlekberliS <117600954+AlekberliS@users.noreply.github.com>
2024-08-14 15:07:16 +0400

    shortcomings-12

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,17 @@ module.exports = {
     extend: {
       width: {
         '98': '360px',
-        
+        '86p':"86%",
+        '94p':"94%",
+        '70p':'70%',
+        '350':"330px",
       },
       borderWidth: {
         't-1': '1px', // Custom border-top width of 1px
       },
       colors: {
         borderCol: '#192038',
+        textCol:'#c5cee0',
       },
     },
   },
