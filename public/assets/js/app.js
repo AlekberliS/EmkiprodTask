d0e88102a2de3d4fc0a648083c330f50805b13f0
Sevinc Abbasova <abbasovasevinc889@gmail.com>
2024-08-16 18:25:58 +0400

    aboutpage

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -70,3 +70,13 @@ var swiper = new Swiper(".mySwiper", {
       }
     },
   });
+
+  //scroll
+  function scrolll() {
+    var left = document.querySelector(".scroll-images");
+    left.scrollBy(350,0);
+  }
+  function scrollr() {
+    var right = document.querySelector(".scroll-images");
+    right.scrollBy(-350,0);
+  }
